531b0f21ea313144872f9add658851d728291bd9
Eglegg18 <44852810+Eglegg18@users.noreply.github.com>
2018-12-10 17:32:13 -0500

    Add files via upload

---
+++ 121018PythonFinal.py
@@ -0,0 +1,132 @@
+Python 2.7.13 (v2.7.13:a06454b1afa1, Dec 17 2016, 20:42:59) [MSC v.1500 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+ RESTART: P:\Fall2018\GISC 3200K - Python Programming for GIS\PythonFinalProjectShell.py 
+>>> 
+ RESTART: P:\Fall2018\GISC 3200K - Python Programming for GIS\PythonFinalProjectShell.py 
+>>> run_test(questions)
+What color are apples?
+(a) Red/Green
+(b) Purple
+(c) Orange
+
+a
+
+Traceback (most recent call last):
+  File "<pyshell#0>", line 1, in <module>
+    run_test(questions)
+  File "P:\Fall2018\GISC 3200K - Python Programming for GIS\PythonFinalProjectShell.py", line 24, in run_test
+    answer = input(question.prompt)
+  File "<string>", line 1, in <module>
+NameError: name 'a' is not defined
+>>> 
+ RESTART: P:\Fall2018\GISC 3200K - Python Programming for GIS\PythonFinalProjectShell.py 
+>>> run_test(questions)
+What color are apples?
+(a) Red/Green
+(b) Purple
+(c) Orange
+
+a
+What Color are Bananas?
+(a) Teal\(b) Magenta
+(c) Yellow
+
+a
+What Color are strawberries?
+(a) Yellow
+(b) Red
+(c) Blue
+
+b
+You got 2/3correct
+>>> 
+ RESTART: P:\Fall2018\GISC 3200K - Python Programming for GIS\PythonFinalProjectShell.py 
+What color are apples?
+(a) Red/Green
+(b) Purple
+(c) Orange
+
+a
+What Color are Bananas?
+(a) Teal\(b) Magenta
+(c) Yellow
+
+a
+What Color are strawberries?
+(a) Yellow
+(b) Red
+(c) Blue
+
+a
+You got 1/3correct
+>>> 
+>>> 
+ RESTART: P:\Fall2018\GISC 3200K - Python Programming for GIS\PythonFinalProjectShell.py 
+What color are apples?
+(a) Red/Green
+(b) Purple
+(c) Orange
+
+a
+What Color are Bananas?
+(a) Teal\(b) Magenta
+(c) Yellow
+
+a
+What Color are strawberries?
+(a) Yellow
+(b) Red
+(c) Blue
+
+a
+You got 1/3correct
+a
+>>> 
+>>> a
+
+Traceback (most recent call last):
+  File "<pyshell#4>", line 1, in <module>
+    a
+NameError: name 'a' is not defined
+>>> 
+ RESTART: P:\Fall2018\GISC 3200K - Python Programming for GIS\PythonFinalProjectShell.py 
+What color are apples?
+(a) Red/Green
+(b) Purple
+(c) Orange
+
+a
+What Color are Bananas?
+(a) Teal\(b) Magenta
+(c) Yellow
+
+a
+What Color are strawberries?
+(a) Yellow
+(b) Red
+(c) Blue
+
+a
+You got 1/3correct
+>>> 
+ RESTART: P:\Fall2018\GISC 3200K - Python Programming for GIS\PythonFinalProjectShell.py 
+What color are apples?
+(a) Red/Green
+(b) Purple
+(c) Orange
+
+a
+What Color are Bananas?
+(a) Teal\(b) Magenta
+(c) Yellow
+
+a
+What Color are strawberries?
+(a) Yellow
+(b) Red
+(c) Blue
+
+a
+You got 1/3correct
+>>> 
